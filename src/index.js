1324b7c502c0356d55e25b19940d36ae08b8e074
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2021-12-15 18:21:33 -0500

    Add feature to send data to API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import scoreLibrary from './manage_scores.js'
 // global variables
 const score = new scoreLibrary();
 // query selectors
-
+const scoreForm = document.querySelector('#score_form');
+const playerName = document.querySelector('#name_input');
+const playerScore = document.querySelector('#score_input');
 // functions
 
 // event listeners
+scoreForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  var newScore = {
+    "user": playerName.value,
+    "score": playerScore.value,
+  }
+  score.send(newScore).then(() => {
+    playerScore.value='';
+    playerName.value='';
+  });
+})
--- src/manage_scores.js
+++ src/manage_scores.js
@@ -1,8 +1,18 @@
 export default class scoreLybrary {
   constructor() {
+    this.get()
+  }
+
+  get = async () => {
     fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/th5xBYWSs5VtsOmKTkPd/scores/',).then((response) => response.json())
     .then((json) => this.list=json.result);
+  }
 
-    
+  send = async (newScore) => {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/th5xBYWSs5VtsOmKTkPd/scores/',{
+      method : "POST",
+      body : JSON.stringify(newScore),
+      headers : {'Content-type': 'application/json; charset=UTF-8',}
+    })
   }
 }
