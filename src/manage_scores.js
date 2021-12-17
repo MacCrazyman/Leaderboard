bff3e60d494016945f11c29f3d62a1ef6750c198
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2021-12-17 12:08:36 -0500

    Update readme. Delete comented code

--- src/manage_scores.js
+++ src/manage_scores.js
@@ -5,12 +5,7 @@ export default class ScoreLybrary {
 
   get = async () => {
     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/th5xBYWSs5VtsOmKTkPd/scores/');
-
     return response.json();
-    // .then((json) => {
-    //   this.list = json.result;
-    //   this.list.sort((prev,next) => next.score - prev.score)
-    // });
   }
 
   send = async (newScore) => {
